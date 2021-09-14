c89871ca360edb835c22f4988e2b750521e6dc37
Suraj Rai <77954411+devsurajrai@users.noreply.github.com>
2021-09-14 20:25:28 +0530

    refactor: code review

--- scripts/hypotenuse.js
+++ scripts/hypotenuse.js
@@ -1,38 +1,36 @@
-let questionBox=document.querySelector(".container")
-let refreshBtn=document.querySelector(".container__child--button-refresh")
-let submitBtn=document.querySelector(".container__child--button")
-let inputOne=document.querySelector(".first")
-let inputTwo=document.querySelector(".second")
-let output=document.querySelector(".output")
-let errorInput=document.querySelector(".input-error-text")
-let hypotenuseValue=document.querySelector(".hypotenuse-value")
-let outputSqrt=document.querySelector(".output-sqrt")
+let questionBox = document.querySelector(".container")
+let refreshBtn = document.querySelector(".container__child--button-refresh")
+let submitBtn = document.querySelector(".container__child--button")
+let inputOne = document.querySelector(".first")
+let inputTwo = document.querySelector(".second")
+let output = document.querySelector(".output")
+let errorInput = document.querySelector(".input-error-text")
+let outputSqrt = document.querySelector(".output-sqrt")
+let hypotenuseValue = document.querySelector(".hypotenuse-value")
 
 
 
-function subBtnClickHandler(){
-  let inputValidation=false
-  let twoSidesSqrSum=(parseInt(inputOne.value)*parseInt(inputOne.value))+(parseInt(inputTwo.value)*parseInt(inputTwo.value))
-  let inputArr=[inputOne.value,inputTwo.value]
+function subBtnClickHandler() {
+    let inputValidation = false
+    let twoSidesSqrSum = (parseInt(inputOne.value) * parseInt(inputOne.value)) + (parseInt(inputTwo.value) * parseInt(inputTwo.value))
+    let inputArr = [inputOne.value, inputTwo.value]
 
-  for(let item in inputArr){
-    if(parseInt(inputArr[item])===0 ||inputArr[item]==="" || parseInt(inputArr[item])<0){
-      inputValidation=true
+    for (let item in inputArr) {
+        if (parseInt(inputArr[item]) === 0 || inputArr[item] === "" || parseInt(inputArr[item]) < 0) {
+            inputValidation = true
+        }
     }
-  }
 
-  if(inputValidation!==true){
-  output.value="C"
-  outputSqrt.value=Math.sqrt(twoSidesSqrSum).toFixed(2)
-  hypotenuseValue.classList.add("display-block")
+    if (inputValidation !== true) {
+        output.value = "C"
+        outputSqrt.value = Math.sqrt(twoSidesSqrSum).toFixed(2)
+        hypotenuseValue.classList.add("display-block")
 
-  }
-  else{
-    errorInput.innerHTML="Please Check The Input"
-    errorInput.style.color="Red"
-  }
+    } else {
+        errorInput.innerHTML = "Please Check The Input"
+        errorInput.style.color = "Red"
+    }
 }
-submitBtn.addEventListener('click',subBtnClickHandler)
+submitBtn.addEventListener('click', subBtnClickHandler)
 
-refreshBtn.addEventListener('click',()=>{window.location.reload()})
-
+refreshBtn.addEventListener('click', () => { window.location.reload() })
